882d4a981410df05506738d0d4fe9440c937b7bb
Anderson Pires <andersonpires855@gmail.com>
2022-06-30 14:53:30 -0300

    Solução do desafio 1 ,utilizando o recurso de interface

---
+++ src/desafios/soluçãoDesafio1.ts
@@ -0,0 +1,16 @@
+//Soluão pára o desafio 1 ,usnado oconceito de interface
+
+interface Funcionario {
+    codigo: number,
+    nome: string,
+}
+
+const funcionario: Funcionario = {
+    codigo: 10,
+    nome: "John"
+}
+
+const funcionario2: Funcionario = {
+    codigo: 122,
+    nome: "Anderson"
+}
